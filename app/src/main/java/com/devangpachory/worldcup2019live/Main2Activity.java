6af8a6ad035177c1edcae8df64cb5720b9427b36
devangpachory <devang.pachory_mca18@gla.ac.in>
2019-06-05 11:40:51 +0530

    Initial commit

---
+++ app/src/main/java/com/devangpachory/worldcup2019live/Main2Activity.java
@@ -0,0 +1,22 @@
+package com.devangpachory.worldcup2019live;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class Main2Activity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+
+
+
+    }
+}
